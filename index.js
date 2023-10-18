75a0fbee125e590c2d61e4453b6b642ac14a4908
komiifo <nathan440@live.fr>
2023-10-18 09:30:01 +0400

    init projet + install express

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+
+const app = express();
+
+// Configuration de la vue EJS
+app.set('view engine', 'ejs');
+
+app.use(express.json());
+
+// Middleware pour parser le corps des requêtes
+app.use(express.urlencoded({ extended: true }));
+
+// Accueil - GET
+app.get('/', (req, res) => {
+    res.send('Hello, world!');
+});
+
+// Démarrage du serveur sur le port 3000
+app.listen(3000, (req, res) => {
+    console.log('Serveur à l\'écoute sur le port 3000');
+});
